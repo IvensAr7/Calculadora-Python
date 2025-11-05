63dc0561461910ea61c28d56835ada87b29c9d0a
IvensAr7 <ivens.silva1@aluno.ce.gov.br>
2025-11-05 12:04:33 -0300

    Add files via upload

---
+++ controleViagensPython/programa.py
@@ -0,0 +1,15 @@
+from funcoes import *
+
+
+def main():
+    print('''
+[ 1 ] REGISTRAR NOVA VIAGEM
+[ 2 ] EXIIR TODAS AS VIAGENS
+[ 3 ] BUSCAR VIAGEM POR MOTORISTA
+[ 4 ] EXIBIR VIAGEM MAIS CARA
+[ 5 ] MOSTRAR MÉDIA GERAL DE CONSUMO 
+[ 0 ] SAIR
+''')
+    
+    op = input('Digite a sua opção: ').strip()
+    if op not in 
